23db9965cb0b723241727f96a4ceffb66d149ced
harry524483 <36663642+harry524483@users.noreply.github.com>
2020-07-14 09:39:38 +0100

    Build

---
+++ precache-manifest.9c4b09e107455f54d43187b52b6b5301.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c4f54d79fd79ce64063802a947401d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee787e2af34ef670b23d",
+    "url": "/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "07f4be1ea3ec0828d332",
+    "url": "/static/css/main.9737ce6d.chunk.css"
+  },
+  {
+    "revision": "ee787e2af34ef670b23d",
+    "url": "/static/js/2.9a912d1c.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/static/js/2.9a912d1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f4be1ea3ec0828d332",
+    "url": "/static/js/main.6be2ef42.chunk.js"
+  },
+  {
+    "revision": "dc907f2f3acbe90a2581",
+    "url": "/static/js/runtime-main.c4ada955.js"
+  }
+]);
